1d3dd288e7d60d06c75f8ed68e6374f9e9d7e493
Marvel Zheng <zheng.marvel@gmail.com>
2022-06-26 00:54:19 -0400

    test code

--- econobot.py
+++ econobot.py
@@ -1,7 +1,6 @@
 import discord
 import os
 import krakenex
-import market_depth
 from dotenv import load_dotenv
 from requests.exceptions import HTTPError
 
@@ -10,12 +9,41 @@ load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 bot = discord.Bot()
 
+from requests.exceptions import HTTPError
+import krakenex
+
+def findSubtotals(results):
+    orders = {}
+    for i in results:
+        orders[float(i[0])] = float(i[1])
+
+    sum = 0
+    for value in orders.keys():
+        sum += value
+    marketPrice = sum/50
+
+    total = 0
+    for value in orders.keys():
+        weighted = 1/((abs((value-marketPrice)/marketPrice))+1)*orders[value]
+        total += weighted
+    return total
+
+def findMarketDepth(asks, bids):
+    return findSubtotals(asks) + findSubtotals(bids)
+
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected!')
 
 @bot.command(name="market_depth", description="finds market depth for given pair")
-async def market_depth(ctx, pair):
-    await ctx.respond(market_depth.findMarketDepth(pair))
+async def market_depth(ctx, pair: str):
+    try:
+        response = kraken.query_public('Depth', {'pair': pair, 'count': '500'})
+        bids = response['result'][list(response["result"].keys())[0]]['bids']
+        asks = response['result'][list(response["result"].keys())[0]]['asks']
+
+    except HTTPError as e:
+        await ctx.respond("Oops! An error occurred!")
+    await ctx.respond(findMarketDepth(asks, bids))
 
 bot.run(TOKEN)
