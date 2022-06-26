009325411bc942c18afc1ebc0e63755dd148ecea
Marvel Zheng <zheng.marvel@gmail.com>
2022-06-25 23:45:43 -0400

    add market_depth command

--- econobot.py
+++ econobot.py
@@ -1,15 +1,21 @@
 import discord
 import os
+import krakenex
+import market_depth
 from dotenv import load_dotenv
+from requests.exceptions import HTTPError
 
+kraken = krakenex.API()
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 bot = discord.Bot()
-
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected!')
 
+@bot.command(name="market_depth", description="finds market depth for given pair")
+async def market_depth(ctx, pair):
+    await ctx.respond(market_depth.findMarketDepth(pair))
 
 bot.run(TOKEN)
---
+++ market_depth.py
@@ -0,0 +1,30 @@
+from requests.exceptions import HTTPError
+import krakenex
+
+kraken = krakenex.API()
+
+def findMarketDepth(pair):
+    try:
+        response = kraken.query_public('Depth', {'pair': pair, 'count': '500'})
+        bids = response['result'][list(response["result"].keys())[0]]['bids']
+        asks = response['result'][list(response["result"].keys())[0]]['asks']
+    except HTTPError as e:
+        print(str(e))
+
+    def findSubtotals(results):
+        orders = {}
+        for i in results:
+            orders[float(i[0])] = float(i[1])
+
+        sum = 0
+        for value in orders.keys():
+            sum += value
+        marketPrice = sum/500
+
+        total = 0
+        for value in orders.keys():
+            weighted = 1/((abs((value-marketPrice)/marketPrice))+1)*orders[value]
+            total += weighted
+        return total
+    
+    return(findSubtotals(asks) + findSubtotals(bids))
