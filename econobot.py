efb8cc167fd46a47fed28679d2b510fc8ac9583b
Clara Fang <clara.fang@gmail.com>
2022-06-26 09:31:50 -0400

    add price command

--- econobot.py
+++ econobot.py
@@ -31,9 +31,18 @@ def findSubtotals(results):
 def findMarketDepth(asks, bids):
     return findSubtotals(asks) + findSubtotals(bids)
 
+
+def findAveragePrice(asks):
+    sum = 0
+    for i in asks:
+        sum += float(i[0])
+    return sum / 500
+
+
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected!')
+
 
 @bot.command(name="market_depth", description="finds market depth for given pair")
 async def market_depth(ctx, pair: str):
@@ -45,5 +54,17 @@ async def market_depth(ctx, pair: str):
     except HTTPError as e:
         await ctx.respond("Oops! An error occurred!")
     await ctx.respond(findMarketDepth(asks, bids))
+
+
+@bot.command(name="price", description="finds asking price of given pair")
+async def price(ctx, pair):
+    try:
+        response = kraken.query_public('Depth', {'pair': pair, 'count': '500'})
+        asks = response['result'][list(response["result"].keys())[0]]['asks']
+    except HTTPError as e:
+        print(str(e))
+
+    await ctx.respond(findAveragePrice(asks))
+
 
 bot.run(TOKEN)
